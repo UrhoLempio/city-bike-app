1016a64b6507d0fff41e4c81221aace7f0b0cca3
Urho Lempiö <urholempio@Urhos-MBP.home>
2023-11-15 19:34:32 +0200

    Added more routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,11 +17,29 @@ app.get('/stations', async (req, res) => {
     res.send(users);
 });
 
-app.get('/station/:id', async (req, res) => {
+app.get('/stations/:id', async (req, res) => {
     const id = req.params.id
     const users = await sql`
     SELECT * FROM "station"
     WHERE id = ${id}
+    `
+    res.send(users);
+});
+
+app.get('/stations/:id/departures', async (req, res) => {
+    const id = req.params.id
+    const users = await sql`
+    SELECT * FROM "journey"
+    WHERE departure_station_id = ${id}
+    `
+    res.send(users);
+});
+
+app.get('/stations/:id/returns', async (req, res) => {
+    const id = req.params.id
+    const users = await sql`
+    SELECT * FROM "journey"
+    WHERE return_station_id = ${id}
     `
     res.send(users);
 });
